88120c9202b11e4b1fb1ed6d42f54e9fdbafe40f
Misha <mishkamakarevitch@gmail.com>
2022-01-25 00:58:33 +0200

    modalAddTransaction feat(component) added beta-version of modalAddTransaction and global mui styles

---
+++ src/components/ModalAddTransaction/ModalAddTransaction.js
@@ -0,0 +1,99 @@
+import {Modal, Typography, Box, Switch, Stack, ButtonGroup, Button, Grid, TextField, Select, MenuItem, InputLabel} from '@mui/material';
+import DatePicker from '@mui/lab/DatePicker';
+import ruLocale from 'date-fns/locale/ru';
+import enLocale from 'date-fns/locale/en-US';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import style from './ModalAddTransaction.module.css';
+import StyledSwitch from '../StyledSwitch/StyledSwitch';
+import {useState} from 'react';
+
+function ModalAddTransaction() {
+
+  let categories = ['Main', 'Food', 'Car', 'Development', 'Kids', 'House', 'Education', 'Others'];
+
+  const [value, setValue] = useState(new Date());
+  const [category, setCategory] = useState('');
+  const [income, setIncome] = useState(true);
+
+
+  const changeCategory = (event) => {
+    setCategory(event.target.value)
+  }
+
+  const changeTypeOfTransaction = (event) => {
+    setIncome((income) => !income)
+  }
+
+  return (
+    <Modal
+      open={true}
+      onClose={false}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div className={style.box}>
+        <Typography id="modal-modal-title" variant="h1" component="h1" sx={{mb: '40px'}}>
+          Add transaction
+        </Typography>
+        <Stack direction='row' spacing='20px' alignItems='center' sx={{mb: '38px'}}>
+          <Typography sx={{color: '#24CCA7'}}>
+            Income
+          </Typography>
+          <StyledSwitch
+            onChange={changeTypeOfTransaction}
+          />
+          <Typography sx={{color: '#FF6596'}}>
+            Expenses
+          </Typography>
+        </Stack>
+        <Grid container columnSpacing='30px' rowSpacing='40px' sx={{width: '100%'}}>
+          {income || <Grid item sm={12} xs={12} md={12}>
+              <InputLabel id="selectCategoryLabel">Select a category</InputLabel>
+              <Select
+                labelId="selectCategoryLabel"
+                id="selectCategoryField"
+                value={category}
+                variant='standard'
+                label='Select a category'
+                onChange={changeCategory}
+                sx={{width: '100%'}}
+              >
+                {categories.map((category) => {
+                  return (
+                    <MenuItem value={category}>{category}</MenuItem>
+                  )
+                })}
+              </Select>
+          </Grid>
+          }
+          <Grid item sm={6} xs={6} md={6}>
+            <TextField id='amountField' placeholder='0.00' variant='standard'/>
+          </Grid>
+          <Grid item sm={6} xs={6} md={6}>
+            <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
+              <DatePicker
+                mask='__.__.____'
+                value={value}
+                views={['year', 'month', 'day']}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} id='dateField' variant='standard'/>}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item sm={12} xs={12} md={12}>
+            <TextField id='commentsField' placeholder='Comment' variant='standard' sx={{width: '100%'}}/>
+          </Grid>
+        </Grid>
+        <Stack sx={{mt: '50px'}}>
+          <Button variant='contained' sx={{color: '#fff', mb: '20px'}}>Add</Button>
+          <Button variant='outlined'>Cancel</Button>
+        </Stack>
+      </div>
+    </Modal>
+  );
+}
+
+export default ModalAddTransaction;
