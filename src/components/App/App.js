bcd0a6ba1b7dd9a607a12545358d2fdf6e57f415
Erik Kopcha <erickopcha@gmail.com>
2022-02-01 22:24:19 +0200

    protectedRoute feat: protect dashboardPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,21 +5,24 @@ import LoginPage from '../LoginPage/LoginPage'
 import RegistrationPage from '../RegistrationPage/RegistrationPage';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../../settings/themeSettings';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 const App = () => {
-  const isUserAuthenticated = false; // for test
+  // wait login state
+  const isUserAuthenticated = true;
   const getRedirectUrl = () => isUserAuthenticated ? "/home/" : "/login";
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Routes>
-          <Route
-            exact
-            path="/"
-            element={ <Navigate to={getRedirectUrl()} /> } />
+          <Route exact path="/" element={ <Navigate to={getRedirectUrl()} /> } />
           <Route path="*" element={ <Navigate to={getRedirectUrl()} /> } />
-          <Route path="/home/*" element={ <DashboardPage /> } />
+          <Route path="/home/*" element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          } />
           <Route path="/login" element={ <LoginPage /> } />
           <Route path="/register" element={ <RegistrationPage /> } />
         </Routes>
