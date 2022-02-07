87672ff9c34120ff141d6975da716ac31a446ef3
Erik Kopcha <erickopcha@gmail.com>
2022-02-07 22:31:59 +0200

    dev fix(balance): sum with spaces

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,5 +1,6 @@
 import style from './Balance.module.css';
 import { useSelector } from 'react-redux';
+import { numberWithSpaces } from '../../helpers/helpers';
 
 const Balance = () => {
   const balance = useSelector((state) => state.user.balance)
@@ -13,7 +14,7 @@ const Balance = () => {
     <div className={style.balance}>
       <p className={style.balanceTitle}>Your balance</p>
       <p className={style.balanceSum}>
-        <span className={style.balanceCurrCode}>{balanceInfo.currCode}</span>&nbsp;{balanceInfo.sum}
+        <span className={style.balanceCurrCode}>{balanceInfo.currCode}</span>&nbsp;{numberWithSpaces(balanceInfo.sum)}
       </p>
     </div>
   );
