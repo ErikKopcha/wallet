72e2acb84842b289292543f079416a9ab0d0c983
Misha <mishkamakarevitch@gmail.com>
2022-02-09 20:36:29 +0200

    dashtable started createAsyncThunk

--- src/features/transactions.js
+++ src/features/transactions.js
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { useSelector } from 'react-redux';
 
 // const initialState = [
 //   { date: '22.01.2022', type: false, category: 'Car', comments: 'ghjghjgh', amount: '123' },
@@ -64,54 +63,54 @@ import { useSelector } from 'react-redux';
 //   },
 //   { date: '22.01.2022', type: false, category: 'Car', comments: 'ghjghjgh', amount: '565656' },
 // ];
-const useTransactionsAPI = () => {
-  const urlBase = 'https://wallet.goit.ua/api';
-  const _apiTransactions = 'transactions';
-  const _apiCategories = 'transaction-categories';
-  const token = useSelector(state => state.session.authToken);
+// const useTransactionsAPI = () => {
+//   const urlBase = 'https://wallet.goit.ua/api';
+//   const _apiTransactions = 'transactions';
+//   const _apiCategories = 'transaction-categories';
+//   const token = useSelector(state => state.session.authToken);
+//
+//   const requestOptions = {
+//     headers: {
+//       'Authorization': `Bearer ${token}`,
+//     },
+//   };
+// };
 
-  const requestOptions = {
-    headers: {
-      'Authorization': `Bearer ${token}`,
-    },
-  };
-};
+const initialState = { transactions: [], categories: [], status: null, error: null };
 
 export const fetchCategories = createAsyncThunk(
-  'reansactions/fetchCategories',
+  'transactions/fetchCategories',
   async function() {
-    const token = useSelector(state => state.session.authToken);
+    // const token = useSelector(state => state.session.authToken);
+    const token = localStorage.getItem('token');
     const requestOptions = {
       headers: {
         'Authorization': `Bearer ${token}`,
       },
     };
     const response = await fetch('https://wallet.goit.ua/api/transaction-categories', requestOptions);
-    const data = await response.json();
-
-    return data;
-  }
-)
+    return await response.json();
+  },
+);
 
 export const fetchTransactions = createAsyncThunk(
   'transactions/fetchTransactions',
   async function() {
-    const token = useSelector(state => state.session.authToken);
+    const token = localStorage.getItem('token');
+    //const token = useSelector(state => state.session.authToken);
     const requestOptions = {
       headers: {
         'Authorization': `Bearer ${token}`,
       },
     };
     const response = await fetch('https://wallet.goit.ua/api/transactions', requestOptions);
-    const data = await response.json();
-
-    return data;
+    return await response.json();
   },
 );
 
 export const transactionsSlice = createSlice({
   name: 'transactions',
-  initialState: { transactions: [], categories: [], status: null, error: null },
+  initialState: initialState,
   reducers: {
     // saveCategories: (state, action) => {
     //   state.categories = action.payload;
@@ -132,7 +131,7 @@ export const transactionsSlice = createSlice({
     },
   },
   extraReducers: {
-    [fetchTransactions.pending]: (state, action) => {
+    [fetchTransactions.pending]: (state) => {
       state.status = 'loading';
       state.error = null;
     },
@@ -149,11 +148,11 @@ export const transactionsSlice = createSlice({
         };
       });
     },
-    [fetchTransactions.rejected]: (state, action) => {
-
-    },
+    // [fetchTransactions.rejected]: (state, action) => {
+    //
+    // },
     [fetchCategories.fulfilled]: (state, action) => {
-      state.categories = action.payload;
+      state.categories.push(...action.payload);
     },
   },
 });
