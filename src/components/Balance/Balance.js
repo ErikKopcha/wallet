2949f1dd127d08437003036fdaae41be5e665b61
Erik Kopcha <erickopcha@gmail.com>
2022-02-20 17:38:55 +0200

    dev update: user balance

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -4,21 +4,22 @@ import style from 'components/Balance/Balance.module.css';
 import { numberWithSpaces } from 'helpers/helpers';
 
 const Balance = () => {
-  const balance = useSelector((state) => state.user.balance)
+  const balance = useSelector(state => state.user.balance);
 
   const balanceInfo = {
     sum: balance,
-    currCode: `₴`
+    currCode: `$`,
   };
 
   return (
     <div className={style.balance}>
       <p className={style.balanceTitle}>Your balance</p>
       <p className={style.balanceSum}>
-        <span className={style.balanceCurrCode}>{balanceInfo.currCode}</span>&nbsp;{numberWithSpaces(balanceInfo.sum)}
+        <span className={style.balanceCurrCode}>{balanceInfo.currCode}</span>
+        &nbsp;{numberWithSpaces(balanceInfo.sum)}
       </p>
     </div>
   );
-}
+};
 
 export default Balance;
